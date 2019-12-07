ddbdf128dc3d3e72b71c38e01389b28ee44ab574
DESKTOP-A828S0O\jiwon <C:\Users\\Documents\git_SourceTree>
2019-12-07 13:12:23 +0900

    Create WebController

---
+++ src/main/java/boot/security/oauth2/web/WebController.java
@@ -0,0 +1,27 @@
+package boot.security.oauth2.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping({"","/"})
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){ return "login";}
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(){
+        return "loginSuccess";
+    }
+
+    @GetMapping("/loginFailure")
+    public String loginFailure(){
+        return "loginFailure";
+    }
+
+}
